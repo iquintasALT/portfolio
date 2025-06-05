01f7da9c3b89858ec7d1ed53b4b942e7c73b3f2d
iquintasALT <iquintas@ucm.es>
2025-06-05 15:34:49 +0200

    chore: add remix-themes and setup eslint for TS
    - Install and configure eslint with TypeScript support.
    - Add remix-themes to manage themes and prevent FOUC.

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'react': react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'react/prop-types': 'off',
+      'react-refresh/only-export-components': 'off',
+      'react/no-unescaped-entities': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^@?\\w'],
+            ['^@/'],
+            ['^\\.\\./', '^\\./'],
+            ['^.+\\.?(css)$'],
+          ],
+        },
+      ],
+    },
+  },
+  {
+    ignores: ['dist', '.eslintrc.cjs'],
+  },
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-
+import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   resolve: {
     alias:{
+      /* eslint-disable no-undef */
       "@": path.resolve(__dirname, "./app"),
     }
   }

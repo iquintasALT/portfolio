8bab184a402a8e8daabdd5cb694c10e103fe1129
iquintasALT <iquintas@ucm.es>
2025-07-08 19:29:13 +0200

    feat: rehype-pretty-code highlighted code on mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,40 @@
-import withMDX from "@next/mdx";
+import fs from "node:fs";
+import nextMDX from "@next/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
 
-const mdx = withMDX();
-export default mdx({
-  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
-  images: {
-    domains: ["github.com"],
+/** @type {import('rehype-pretty-code').Options} */
+const options = {
+  theme: "one-dark-pro",
+};
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [[rehypePrettyCode, options]],
   },
 });
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+        port: "",
+        pathname: "/**",
+      },
+      // Add more patterns here if you need to support other GitHub image URLs
+    ],
+  },
+};
+
+export default withMDX(nextConfig);
